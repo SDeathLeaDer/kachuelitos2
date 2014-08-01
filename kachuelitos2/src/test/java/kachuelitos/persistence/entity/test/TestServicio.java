11641280dd0ca32519c85da338ea3c1d85b6c1ab
camaradakoba <camaradakoba@koba>
2014-08-01 00:34:48 -0500

    testservicio

--- kachuelitos2/src/test/java/kachuelitos/persistence/entity/test/TestServicio.java
+++ kachuelitos2/src/test/java/kachuelitos/persistence/entity/test/TestServicio.java
@@ -1,14 +1,26 @@
 package kachuelitos.persistence.entity.test;
 
 import static org.junit.Assert.*;
+import kachuelitos.persistence.entity.Servicio;
 
 import org.junit.Test;
 
 public class TestServicio {
+	private Servicio servicio;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testgetysetServicio() {
+		int idServicio = 111111;
+		String descripcionServicio= "jardineria";
+		String detalleServicio= "jardineria";
+		servicio.setIdServicio(idServicio);
+		servicio.setDetalleServicio(detalleServicio);
+		servicio.setDescripcionServicio(descripcionServicio);
+		
+		
+		assertEquals(servicio.getIdServicio(),idServicio);
+		assertEquals(servicio.getDetalleServicio(),detalleServicio);
+		assertEquals(servicio.getDescripcionServicio(),descripcionServicio);
 	}
 
 }
